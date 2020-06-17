ae058e3531fb5fa443cea2efc64031fe4943238b
tuantrankim <tuantrankim@gmail.com>
2020-06-17 16:22:47 -0700

    Initial project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress from = new MailboxAddress("From test mail",
+            "info@raovatvietmy.com");
+            message.From.Add(from);
+
+            MailboxAddress to = new MailboxAddress("Tuan Tran",
+            "tuantrankim@gmail.com");
+            message.To.Add(to);
+
+            message.Subject = "Testing email from tuan";
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = "<h1>Hello World!</h1>";
+            bodyBuilder.TextBody = "Hello World!";
+            
+            //bodyBuilder.Attachments.Add(env.WebRootPath + "\\file.png");
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("email-smtp.us-west-2.amazonaws.com", 465, true);
+
+            //Update username and password to run testing
+            client.Authenticate("username", "password");
+
+            client.Send(message);
+            client.Disconnect(true);
+            client.Dispose();
+            
+            Console.WriteLine("Done.");
+        }
+    }
+}
